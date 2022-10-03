665b7e26b5ff6210b3bcac1ac3b791daada7efa0
felipr <100290286+felipehdev@users.noreply.github.com>
2022-10-03 18:20:29 -0300

    allow orgin restringido

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(express.json());
 //midleware cors
 // MODIFICAR QUEM PODE FAZER REQUISIÇAO ANTES DE LANÇAR
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000/");
+    res.header("Access-Control-Allow-Origin", "http://felipr.com/");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     app.use(cors());
     next();
