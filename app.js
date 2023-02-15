e7612d46af4fe5f4682f86c37def83b5919f133c
felipr <100290286+felipehdev@users.noreply.github.com>
2023-02-15 10:46:43 -0300

    modificado cors para deploy

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use('/files', express.static(path.resolve(__dirname, "src")));
 
 //https://cartoes.felipr.com, 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://cartoes.felipr.com");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     app.use(cors());
     next();
